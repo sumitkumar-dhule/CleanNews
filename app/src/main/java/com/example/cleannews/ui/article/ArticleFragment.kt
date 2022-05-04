2e01225bbe43f2fe7378cd5a2ed63cc7503bcaa0
Sumitkumar <sumitkumardhule@gmail.com>
2022-05-04 18:50:09 +0530

    Initial setup

---
+++ app/src/main/java/com/example/cleannews/ui/article/ArticleFragment.kt
@@ -0,0 +1,7 @@
+package com.example.cleannews.ui.article
+
+import androidx.fragment.app.Fragment
+import com.example.cleannews.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+}
---
+++ app/src/main/java/com/example/cleannews/ui/breakingnews/BreakingNewsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.cleannews.ui.breakingnews
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.cleannews.databinding.FragmentBreakingNewsBinding
+
+class BreakingNewsFragment : Fragment() {
+
+    private var _binding: FragmentBreakingNewsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(BreakingNewsViewModel::class.java)
+
+        _binding = FragmentBreakingNewsBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
