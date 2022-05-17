7ffab46ba6d13fead9e43c418d2b65e432e5b72a
Sumitkumar <sumitkumardhule@gmail.com>
2022-05-17 10:40:23 +0530

    Refactor Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,11 +60,11 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
 
     // Room
-    implementation ("androidx.room:room-runtime:2.2.5")
-    kapt ("androidx.room:room-compiler:2.2.5")
+    implementation (Room.roomRuntime)
+    kapt (Room.roomCompiler)
 
     // Kotlin Extensions and Coroutines support for Room
-    implementation ("androidx.room:room-ktx:2.2.5")
+    implementation (Room.roomKtx)
 
     // Coroutines
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
@@ -75,17 +75,17 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
 
     // Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.6.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.6.0")
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.5.0")
+    implementation (Retrofit.retrofit)
+    implementation (Retrofit.gsonConverter)
+    implementation (Retrofit.okHttpLoggingInterceptor)
 
     // Navigation Components
     implementation ("androidx.navigation:navigation-fragment-ktx:2.2.1")
     implementation ("androidx.navigation:navigation-ui-ktx:2.2.1")
 
     // Glide
-    implementation ("com.github.bumptech.glide:glide:4.11.0")
-    kapt ("com.github.bumptech.glide:compiler:4.11.0")
+    implementation (Glide.gilde)
+    kapt (Glide.gildeCompiler)
 
     implementation(project(Modules.core))
     implementation(project(Modules.breakingNews))
