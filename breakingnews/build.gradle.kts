02cf13147ca74038b0ac94facec61a9c62020f7a
Sumitkumar <sumitkumardhule@gmail.com>
2022-05-18 12:10:32 +0530

    Refactor gradle files

--- breakingnews/build.gradle.kts
+++ breakingnews/build.gradle.kts
@@ -4,15 +4,5 @@ apply {
 
 dependencies {
     "implementation"(project(Modules.core))
-    // Coroutine Lifecycle Scopes
-    "implementation" ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
-    "implementation" ("androidx.lifecycle:lifecycle-runtime-ktx:2.2.0")
-
-    // Navigation Components
-    "implementation" ("androidx.navigation:navigation-fragment-ktx:2.2.1")
-    "implementation" ("androidx.navigation:navigation-ui-ktx:2.2.1")
-
     "implementation"(project(Modules.common))
-
-
 }
