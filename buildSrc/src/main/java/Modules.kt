abcbffc9d413a845aca21f51d2e1608876d0c216
Sumitkumar <sumitkumardhule@gmail.com>
2022-05-19 17:00:17 +0530

    Remove commented code

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,10 +1,7 @@
 object Modules {
     const val app = ":app"
-
     const val core = ":core"
-
     const val breakingNews = ":breakingnews"
     const val savedNews = ":savednews"
-
     const val common = ":common"
 }
