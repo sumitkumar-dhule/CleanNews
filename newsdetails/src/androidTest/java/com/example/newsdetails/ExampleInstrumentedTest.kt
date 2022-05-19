b40aba87eb52468a254ffdbd01d34711d1c3398f
Sumitkumar <sumitkumardhule@gmail.com>
2022-05-19 17:21:39 +0530

    Rename common module to newsDetails

--- newsdetails/src/androidTest/java/com/example/newsdetails/ExampleInstrumentedTest.kt
+++ newsdetails/src/androidTest/java/com/example/newsdetails/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.common
+package com.example.newsdetails
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
--- newsdetails/src/main/java/com/example/newsdetails/ArticleNewsViewModel.kt
+++ newsdetails/src/main/java/com/example/newsdetails/ArticleNewsViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.common
+package com.example.newsdetails
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
